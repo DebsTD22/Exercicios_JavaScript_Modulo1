0ea937cbad0512d78056b1fe43d87655fca7e923
DebsTD22 <dominguetidebora@gmail.com>
2024-07-09 15:43:54 -0300

    exercicio feito

---
+++ exercicio6.js
@@ -0,0 +1,9 @@
+/* EXERCIO PARA ENTENDER NA PRATICA COMO A FUNCTION FUNCIONA
+    Crie uma função que escreva seu nome */
+
+
+
+function escrevaMeuNome(nome){
+    console.log('meu nome é ' + nome);
+}
+escrevaMeuNome('Débora');
