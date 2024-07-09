b41ce976a9ed25d853ad9f35e1250410448089a3
DebsTD22 <dominguetidebora@gmail.com>
2024-07-09 15:58:09 -0300

    exercicio feito

---
+++ exercicio7.js
@@ -0,0 +1,14 @@
+/*
+    Crie uma função que diz se sou maior de idade
+*/
+
+(function verifiqueIdade(idade){
+    if (idade >= 18) {
+        console.log('é maior de idade');
+    } else {
+        console.log('é menor de idade');
+    }
+})(10);
+
+
+
