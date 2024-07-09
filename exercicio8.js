f450dd06ae7f9c320b7b847ea0ceea860b7f2c8f
DebsTD22 <dominguetidebora@gmail.com>
2024-07-09 16:09:38 -0300

    exercicio completo

---
+++ exercicio8.js
@@ -0,0 +1,16 @@
+/* 
+    Crie uma função invocando dentro de outra função
+*/
+
+function escrevaMeuNome(nome){
+    return 'meu nome é ' + nome;
+}
+
+
+(function verifiqueIdade(idade){
+    if (idade >= 18) {
+        console.log( escrevaMeuNome('Débora') + ' sou maior de idade');
+    } else {
+        console.log( escrevaMeuNome('Débora') + ' sou menor de idade');
+    }
+})(18);
