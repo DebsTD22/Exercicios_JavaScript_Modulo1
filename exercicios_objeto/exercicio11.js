d0e9d6f25c36fd9744838fa304a38795f690e9f4
DebsTD22 <dominguetidebora@gmail.com>
2024-07-18 14:08:25 -0300

    indice do exercicio

---
+++ exercicios_objeto/exercicio11.js
@@ -0,0 +1,8 @@
+/* 
+    Crie uma classe para representar pessoas.
+    Para cada pessoa teremos os atributos nome, peso e altura.
+    As pessoas devem ter a capacidade de dizer o valor de seu IMC.
+    Instancie uma pessoa chamada José que tenha 70Kg e 1,75 de altura, e peça para ele dizer o valor de seu IMC.
+*/
+
+
