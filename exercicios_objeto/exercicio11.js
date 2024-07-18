3b80e3a2e1c483d7c5c23b96845cfc692ecfe6a8
DebsTD22 <dominguetidebora@gmail.com>
2024-07-18 14:22:29 -0300

    resolução

--- exercicios_objeto/exercicio11.js
+++ exercicios_objeto/exercicio11.js
@@ -5,4 +5,21 @@
     Instancie uma pessoa chamada José que tenha 70Kg e 1,75 de altura, e peça para ele dizer o valor de seu IMC.
 */
 
+class Pessoa {
+    nome;
+    peso;
+    altura;
+    constructor(nome, peso, altura){
+        this.nome = nome;
+        this.peso = peso;
+        this.altura = altura;
+    }
+    calcularIMC(peso, altura, nome){
+        const icm = this.peso / (this.altura * this.altura);
+        console.log(`Sou ${this.nome}, e meu ICM é ${icm}`);
+    }
+}
 
+const José = new Pessoa('José', 70, 1.75);
+
+José.calcularIMC();
